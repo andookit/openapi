e71a8e15594de6b1637437236d01c400a51915a8
Joseph Carroll <jdsalingerjr@gmail.com>
2022-10-11 11:53:46 -0700

    first build

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   schemas: {
-    // ["api.andook.com.deref"]: require("./generated/api.andook.com.deref.json"),
-    ["api.andook.com"]: require("./generated/api.andook.com.json"),
-  },
+    ['api.andook.com']: require('./generated/api.andook.com.json')
+  }
 };
